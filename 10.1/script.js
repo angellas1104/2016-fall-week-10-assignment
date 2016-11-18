04b6aa559eef9eee9b237d63262c5ac8d6e103fa
liuanqi <liuanqi@liuanqideair.fios-router.home>
2016-11-17 20:25:57 -0500

    week10

--- 10.1/script.js
+++ 10.1/script.js
@@ -12,13 +12,24 @@ var plot = d3.select('.canvas')
     .attr('transform','translate('+ m.l+','+ m.t+')');
 
 //Mapping specific functions
-//Projection
 
-//Geopath
+//Projection
+var projection=d3.geoAzimuthalEqualArea();
+var pathGenerator=d3.geoPath()
+	.projection(projection);
 
 d3.json('../data/world-50m.json',dataloaded);
 
 function dataloaded(err, data){
     console.log(data); //This is a Topojson data
     console.log(topojson.feature(data,data.objects.countries)); //This is the converted GeoJSON data of countries
+var newdata=topojson.feature(data,data.objects.countries);
+    console.log(newdata);
+    projection.fitExtent([[0,0],[w,h]],newdata);
+    var states=plot.selectAll('.state')
+        .data(newdata.features)
+        .enter()
+        .append('path').attr('class','state')
+        .attr('d', pathGenerator);
+
 }
